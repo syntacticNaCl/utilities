5a5167fe2af817a952085f3cb002997fe8a4341f
Garrett Rappaport <garrettr@sideways8.com>
2015-07-13 11:21:04 -0400

    Updated options and used dummy data.

--- php/imgreplace.php
+++ php/imgreplace.php
@@ -1,12 +1,16 @@
 <?php
 /**
  * Strips images from a string of text
- * @param  String $string Full string to parse
- * @param  String $start  Where to start
- * @param  String $end    Where to end
- * @return String         String - images
+ * @param  String $string  Full string to parse
+ * @param  String $start   Where to start
+ * @param  String $end     Where to end
+ * @param  String $options Return found string or parsed string
+ * @return String          String - images
+ *
+ * Base code for this was taken from Justin Cook
+ * http://www.justin-cook.com/wp/2006/03/31/php-parse-a-string-between-two-strings/
  */
-function strip_images ($string, $start, $end) {
+function strip_images ($string, $start, $end, $options = false) {
 	$ltr_count = strlen($start);
 	$string = " ".$string;
     $ini = strpos($string,$start);
@@ -15,9 +19,13 @@ function strip_images ($string, $start, $end) {
     $ini += strlen($start);
     $len = strpos($string,$end,$ini) - $ini;
     $to_replace = substr($string,$ini,$len);
+    if ($options) {
+    	return $to_replace;
+    }
     return str_replace($to_replace, '', $string);
+    
 }
 
-$string = '<p>This is some text here<a href="/floorplans/05302570/"><img src="/uploads/pagesfiles/99.jpg" border="0" alt="Luxury House Plans" title="Luxury House Plans" hspace="5" width="330" height="158" align="right"></a> </p>';
+$string = '<p>This is some text here<a href="/blahblah/2349821349/"><img src="/uploads/blahblah/12.jpg" border="0" alt="Random Image To Replace" title="Random Image To Replace" hspace="5" width="330" height="158" align="right"></a> </p>';
 $parsed = strip_images($string,'<a','</a>');
 echo $parsed;
